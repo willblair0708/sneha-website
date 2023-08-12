e9a5d19c45994b94fd4844cf24042a33ad729efe
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-08-12 08:49:41 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,22 +105,6 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Ellie Chen</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
-        </a>
       </StyledCredit>
     </StyledFooter>
   );
