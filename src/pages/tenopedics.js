b2046b7d4d6782358f7ecd479f6e916cc5d90418
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-11-04 20:44:14 -0700

    Update tenopedics.js

--- src/pages/tenopedics.js
+++ src/pages/tenopedics.js
@@ -1,79 +1,88 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import { Helmet } from 'react-helmet';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { navDelay } from '@utils';
 import { Layout } from '@components';
 import { usePrefersReducedMotion } from '@hooks';
-// import coverImage from '../images/cover-tenopedics.jpg'; // Ensure you have an image at this path
 
+// Styled Components
 const StyledMainContainer = styled.main`
-  ${({ theme }) => theme.mixins.flexCenter};
+  display: flex;
   flex-direction: column;
-  padding: 1rem;
+  padding: 0 2rem;
   max-width: 1200px;
-  margin: 0 auto;
+  margin: 5rem auto;
   margin-top: 100px;
+  opacity: 0;
+  transform: translateY(20px);
+  animation: fadeInUp 0.3s ease forwards;
+  animation-delay: ${({ animationDelay }) => animationDelay || '0.5s'};
+  transition: all 0.3s ease-in-out;
+  will-change: transform, opacity;
 
-  @media (max-width: 768px) {
-    margin-top: 40px;
-  }
-
-  @media (max-width: 480px) {
-    margin-top: 20px;
+  @keyframes fadeInUp {
+    0% {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
   }
 `;
 
 const StyledTitle = styled.h1`
   color: var(--green);
-  font-family: var(--font-mono);
-  font-size: clamp(1.5rem, 5vw, 3rem);
-  line-height: 1.1;
+  font-size: 4rem;
+  margin-bottom: 2rem;
+  text-align: center;
+  font-weight: bold;
 `;
 
 const StyledSubtitle = styled.h2`
-  font-size: clamp(1rem, 2.5vw, 1.5rem);
-  font-weight: 400;
-  line-height: 1.3;
-  margin-bottom: 10px;
-`;
-
-const StyledHomeButton = styled(Link)`
-  ${({ theme }) => theme.mixins.bigButton};
-  margin-top: 30px; // Adjust as needed
-`;
-
-const StyledSection = styled.section`
-  margin-bottom: 20px; // Adjust as needed
+  color: var(--slate);
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+  font-weight: 300;
 `;
 
 const StyledParagraph = styled.p`
-  margin-bottom: 5px;
+  font-size: 1.2rem;
+  color: var(--slate);
+  line-height: 1.7;
+  margin-bottom: 1.5rem;
+  text-align: justify;
+`;
+
+const StyledButton = styled(Link)`
+  ${({ theme }) => theme.mixins.bigButton};
+  margin-top: 40px;
+`;
+
+// Additional styled components for new content
+const StyledSectionTitle = styled.h3`
+  color: var(--slate);
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
 `;
 
 const StyledList = styled.ul`
-  list-style: inside disc;
-  padding: 0;
-  margin-bottom: 10px; // Adjust as needed
+  font-size: 1rem;
+  color: var(--slate);
+  line-height: 1.5;
+  list-style-position: inside;
+  margin-bottom: 2rem;
 `;
 
 const StyledListItem = styled.li`
-  margin-bottom: 10px;
+  margin-bottom: 0.5rem;
 `;
 
 const StyledTeamContainer = styled.div`
-  margin-top: 20px; // Adjust as needed
-`;
-
-const StyledTeamTitle = styled.h3`
-  font-weight: 600;
-  margin-bottom: 10px;
-`;
-
-const StyledTeamText = styled.p`
-  margin-bottom: 10px;
+  margin-top: 2rem;
 `;
 
 const TenopedicsPage = ({ location }) => {
@@ -87,13 +96,18 @@ const TenopedicsPage = ({ location }) => {
 
     const timeout = setTimeout(() => setIsMounted(true), navDelay);
     return () => clearTimeout(timeout);
-  }, []);
+  }, [prefersReducedMotion]);
 
-  const content = (
-    <StyledMainContainer className="fillHeight">
-      <StyledTitle>Tenopedics Project</StyledTitle>
-
-      <StyledSection>
+  return (
+    <Layout location={location}>
+      <Helmet title="Tenopedics Project" />
+      <StyledMainContainer
+        style={{ 
+          animationDelay: prefersReducedMotion ? '0s' : '0.5s',
+          opacity: prefersReducedMotion ? 1 : 0
+        }}
+      >
+        <StyledTitle>Tenopedics Project</StyledTitle>
         <StyledSubtitle>Background</StyledSubtitle>
         <StyledParagraph>
           Arthroscopic Biceps Tenodesis (ABT) holds great promise for patients suffering from biceps
@@ -101,80 +115,32 @@ const TenopedicsPage = ({ location }) => {
           astonishingly underutilized due to the challenges associated with the existing surgical
           tools.
         </StyledParagraph>
+        <StyledSectionTitle>Key user needs:</StyledSectionTitle>
+        <StyledList>
+          <StyledListItem>Compatibility: Surgeons need tools that are designed specifically for the arthroscopic biceps space. Our solution provides instruments that fit this confined area, making the procedure smoother.</StyledListItem>
+          <StyledListItem>Reduced Procedural Time: Streamlining the procedure with efficient tools can significantly decrease surgical time, leading to reduced swelling, lower costs, and increased operative capacity.</StyledListItem>
+          <StyledListItem>Mitigated Fixation-Strength Related Failure: Our solution minimizes the risk of fixation-strength related failures, ensuring better patient outcomes.</StyledListItem>
+        </StyledList>
+        
+        <StyledSectionTitle>Potential Impacts:</StyledSectionTitle>
         <StyledParagraph>
-          Our team sees an opportunity to revolutionize ABT by developing a compact and efficient
-          system tailored to the arthroscopic biceps space. This innovation has the potential to
-          impact both clinicians and patients significantly.
+          By addressing these user needs and bringing innovation to the ABT procedure, we anticipate several significant impacts:
         </StyledParagraph>
-      </StyledSection>
-
-      <StyledSection>
-        <StyledSubtitle>Key User Needs</StyledSubtitle>
         <StyledList>
-          <StyledListItem>
-            Compatibility: Surgeons need tools that are designed specifically for the arthroscopic
-            biceps space.
-          </StyledListItem>
-          <StyledListItem>
-            Reduced Procedural Time: Streamlining the procedure with efficient tools can
-            significantly decrease surgical time.
-          </StyledListItem>
-          <StyledListItem>
-            Mitigated Fixation-Strength Related Failure: Secure fixation of the biceps tendon is
-            crucial for ABT success.
-          </StyledListItem>
+          <StyledListItem>Enhanced Patient Outcomes: Shorter procedural times and improved fixation can lead to quicker patient recovery and better post-operative results.</StyledListItem>
+          <StyledListItem>Reduced Healthcare Costs: Efficiency can lower costs for hospitals and patients, making ABT more accessible and affordable.</StyledListItem>
+          <StyledListItem>Clinician Adoption: A dedicated solution that facilitates the clinician's work and increases efficiency can encourage broader acceptance of ABT.</StyledListItem>
+          <StyledListItem>Innovation in the Field: Our project may set a new standard for ABT procedures, inspiring further innovations in orthopedic surgery.</StyledListItem>
         </StyledList>
-      </StyledSection>
-
-      <StyledSection>
-        <StyledSubtitle>Potential Impacts</StyledSubtitle>
-        <StyledList>
-          <StyledListItem>
-            Enhanced Patient Outcomes: Improved fixation and shorter procedural times can lead to
-            quicker patient recovery.
-          </StyledListItem>
-          <StyledListItem>
-            Reduced Healthcare Costs: More efficient procedures can lower costs for hospitals and
-            patients.
-          </StyledListItem>
-          <StyledListItem>
-            Clinician Adoption: A dedicated solution can encourage wider adoption of ABT among
-            clinicians.
-          </StyledListItem>
-          <StyledListItem>
-            Innovation in the Field: Our project could set a new standard for ABT procedures.
-          </StyledListItem>
-        </StyledList>
-      </StyledSection>
-
-      <StyledTeamContainer>
-        <StyledTeamTitle>Team</StyledTeamTitle>
-        <StyledTeamText>
-          I work with a team of 5 students sponsored by Johns Hopkins Biomedical Engineering. Our
-          project mentors are from the Johns Hopkins University Biomedical Engineering Department,
-          Johns Hopkins Medical Institution, and University of Maryland Medical Institution.
-        </StyledTeamText>
-      </StyledTeamContainer>
-
-      <StyledHomeButton to="/">Go Home</StyledHomeButton>
-    </StyledMainContainer>
-  );
-
-  return (
-    <Layout location={location}>
-      <Helmet title="Tenopedics Project" />
-
-      {prefersReducedMotion ? (
-        <>{content}</>
-      ) : (
-        <TransitionGroup component={null}>
-          {isMounted && (
-            <CSSTransition timeout={500} classNames="fadeup">
-              {content}
-            </CSSTransition>
-          )}
-        </TransitionGroup>
-      )}
+        
+        <StyledSectionTitle>Team:</StyledSectionTitle>
+        <StyledTeamContainer>
+          <StyledParagraph>
+            I work with a team of 5 students sponsored by Johns Hopkins Biomedical Engineering. Our project mentors are from the Johns Hopkins University Biomedical Engineering Department, Johns Hopkins Medical Institution, and University of Maryland Medical Institution.
+          </StyledParagraph>
+        </StyledTeamContainer>
+        <StyledButton to="/">Go Home</StyledButton>
+      </StyledMainContainer>
     </Layout>
   );
 };
